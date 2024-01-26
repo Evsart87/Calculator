d342e045b42d360dac91514291a2430d829d4c20
Evsart87 <evsart1987@gmail.com>
2024-01-26 20:40:53 +0500

    continue...

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import './App.css';
 import design from './images/design.jpg'
 
 function App() {
+  const handleClick = (arg) => {
+    console.log(arg);
+  }
   return (
     <div className='content_center'>
-      {/* <img src={design}></img> */}
+      <img src={design}></img>
       <div className='calc_container'>
-        
+        <div className='upper_part'>kek</div>
+        <div className='bottom_part'>
+          <div className='line'>
+            <div className='math_button' onClick={()=>handleClick('reset')}>AC</div>
+            <div className='math_button' onClick={()=>handleClick('plus_minus')}>+/-</div>
+            <div className='math_button' onClick={()=>handleClick('%')}>%</div>
+            <div className='math_button' onClick={()=>handleClick('/')}>/</div>
+          </div>
+          <div className='line'></div>
+          <div className='line'></div>
+          <div className='line'></div>
+          <div className='line'></div>
+        </div>
       </div>
     </div>
   );
